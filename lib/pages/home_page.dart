53bc34ffde10c7423a649312c7b4913a62180d47
AbhinandhanaPU <abhinandhanapu81@gmail.com>
2024-03-30 23:55:52 +0530

    home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'package:my_portfolio/constants/size.dart';
 import 'package:my_portfolio/widgets/drawer_mobile.dart';
 import 'package:my_portfolio/widgets/header/header_desktop.dart';
 import 'package:my_portfolio/widgets/header/header_mobile.dart';
+import 'package:my_portfolio/widgets/home/home_desktop.dart';
+import 'package:my_portfolio/widgets/home/home_mobile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,9 +18,6 @@ class _HomePageState extends State<HomePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    final screenWidth = screenSize.width;
-
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
         key: scaffoldKey,
@@ -30,28 +29,37 @@ class _HomePageState extends State<HomePage> {
                   scaffoldKey.currentState?.closeEndDrawer();
                 },
               ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            children: [
-              // header
-              if (constraints.maxWidth >= minDesktopWidth)
-                HeaderDesktop()
-              else
-                HeaderMobile(
-                  onLogoTap: () {},
-                  onMenuTap: () {
-                    scaffoldKey.currentState?.openEndDrawer();
-                  },
-                )
-              // home
-              // about
-              // projects
-              // education
-              // certifications
-              // contact
-            ],
-          ),
+        body: Column(
+          children: [
+            // header
+            if (constraints.maxWidth >= minDesktopWidth)
+              HeaderDesktop()
+            else
+              HeaderMobile(
+                onLogoTap: () {},
+                onMenuTap: () {
+                  scaffoldKey.currentState?.openEndDrawer();
+                },
+              ),
+            Expanded(
+              child: PageView(
+                scrollDirection: Axis.vertical,
+                physics: AlwaysScrollableScrollPhysics(),
+                children: [
+                  // home
+                  if (constraints.maxWidth >= minDesktopWidth)
+                    HomeDesktop()
+                  else
+                    HomeMobile(),
+                  // about
+                  // projects
+                  // education
+                  // certifications
+                  // contact
+                ],
+              ),
+            ),
+          ],
         ),
       );
     });
---
+++ lib/widgets/home/home_desktop.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/constants/color_const.dart';
+
+class HomeDesktop extends StatefulWidget {
+  const HomeDesktop({super.key});
+
+  @override
+  State<HomeDesktop> createState() => _HomeDesktopState();
+}
+
+class _HomeDesktopState extends State<HomeDesktop> {
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final screenHeight = screenSize.height;
+
+    return Container(
+      height: screenHeight / 1.2,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 20.0,
+      ),
+      constraints: const BoxConstraints(
+        minHeight: 350.0,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // intro message
+              const Text(
+                "Hi,\nI'm Abhinandhana P U\nA Flutter Developer",
+                style: TextStyle(
+                  fontSize: 30,
+                  height: 1.5,
+                  fontWeight: FontWeight.bold,
+                  color: CustomColor.whitePrimary,
+                ),
+              ),
+              const SizedBox(height: 40),
+              // resume btn
+              Container(
+                width: 200.0,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    colors: [
+                      CustomColor.bluePrimary,
+                      CustomColor.redPrimary,
+                    ],
+                  ),
+                ),
+                child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Resume",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: CustomColor.whitePrimary),
+                    )),
+              )
+            ],
+          ),
+          // image container
+          Container(
+            height: 280,
+            width: 250,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  colors: [
+                    CustomColor.bluePrimary,
+                    CustomColor.redPrimary,
+                  ],
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: CustomColor.bluePrimary,
+                    offset: Offset(-2, 0),
+                    blurRadius: 20,
+                  ),
+                  BoxShadow(
+                    color: CustomColor.redPrimary,
+                    offset: Offset(2, 0),
+                    blurRadius: 20,
+                  ),
+                ]),
+            child: Container(
+              margin: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: CustomColor.bgLight1,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Image.asset(
+                'assets/my_portfolio_profile.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
